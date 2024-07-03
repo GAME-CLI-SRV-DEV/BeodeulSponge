9e9516bfa3f23caae74551263790fb46582e4de9
Outfit8TSB <sunnybunnies09@naver.com>
2024-07-03 19:39:37 +0900

    Update clone.sh

--- script/clone.sh
+++ script/clone.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/SpongePowered/Sponge.git
+# Set the prompt for the shell
+PS1="$"
+basedir="$(cd "$1" && pwd -P)"
+workdir="$basedir/work"
+gpgsign="$(git config commit.gpgsign || echo "false")"
+
+git clone --recursive https://github.com/SpongePowered/Sponge.git
+cd Sponge
+cp scripts/pre-commit .git/hooks
+cd ..
